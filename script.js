4f75008f2d02b9a53ead704b47a10f948ff8932d
rakib <mdrakib991943@.com>
2026-03-03 22:42:56 +0600

    added simple loading

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const createElemets=(anonyns)=>{
+const htmlElements = anonyns.map(anonyn=>  `  <span class ='btn'> ${anonyn} </span>  `);
+  return htmlElements.join(" ");
+}
+
+const manageSpinner=((status)=>{
+    if(status){
+        document.getElementById('spinner').classList.remove('hidden');
+        document.getElementById('word-container').classList.add('hidden');
+    }
+    else{
+           document.getElementById('spinner').classList.add('hidden');
+        document.getElementById('word-container').classList.remove('hidden');
+    }
+})
+
 // Load all lessons
 const loadLessons = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/levels/all');
@@ -5,14 +21,63 @@ const loadLessons = async () => {
     displayLesson(lessons.data);
 }
 
+const removeActive =()=>{
+    const lessonsButtons = document.querySelectorAll('.lessons-btn');
+    lessonsButtons.forEach((btn)=>{
+       btn.classList.remove("btn-secondary")
+    })
+}
+
 // Load words for a specific level
 const loadLevelWord = async (id) => {
+    manageSpinner(true);
+    removeActive();
     const res = await fetch(`https://openapi.programming-hero.com/api/level/${id}`);
     const words = await res.json();
+    const clickBtn = document.getElementById(`lesson-btn-${id}`);
+    clickBtn.classList.remove("btn-outline");
+    clickBtn.classList.add("btn-secondary");
     displayWord(words.data);
 }
 
+const loadWordDetails= async(id)=>{
+   const res = await fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+   const wordDetails = await res.json();
+   DisplayWordDetails(wordDetails.data);
+}
 
+
+
+
+const DisplayWordDetails =(word)=>{
+   const wordsDetails = document.getElementById('words-details');
+   wordsDetails.innerHTML ='';
+   const div = document.createElement('div');
+   div.classList.add('space-y-3')
+   div.innerHTML=`
+   <h2 class="card-title text-xl font-bold"> ${word.word} ||  <i class="fa-solid text-2xl font-bold fa-microphone-lines"></i>${word.pronunciation ? word.pronunciation :"sorry"}</h2>
+    <p> Meaning and Pronunciation</>
+    <p class='font-bangla text-xl font-semibold'>${word.meaning  ? word.meaning:"sorry"} || ${word.pronunciation}</p>
+
+    <div class='space-y-3' > 
+     <h2 class='Font Bold'>Example</h2>
+     <p class='font-bold'>${word.sentence}</p>
+    </div>
+     
+    <div class ='space-y-3'>
+    <h2 class='font-bold'>Synonym</h2>
+       <div class =''>
+       ${createElemets(word.synonyms)}
+       </div>
+    <div/>
+   `
+
+   wordsDetails.appendChild(div);
+   my_modal_5.showModal()
+   
+   
+  
+}
 
 // Display words dynamically
 const displayWord = (words) => {
@@ -26,6 +91,7 @@ const displayWord = (words) => {
             <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
         </div>
     `
+    manageSpinner(false)
     return ;
  }
     words.forEach(word => {
@@ -33,20 +99,21 @@ const displayWord = (words) => {
         const div = document.createElement("div");
         div.innerHTML = `
             <div class="card bg-base-100 w-96 shadow-sm">
-  <div class="card-body">
+    <div class="card-body">
     <h2 class="card-title text-xl font-bold">${word.word ? word.word :"sorry"}</h2>
     <p> Meaning and Pronunciation</>
     <p class='font-bangla text-xl font-semibold'>${word.meaning  ? word.meaning:"sorry"} || ${word.pronunciation}</p>
 
     <div class='flex justify-between items-center mt-2'>
-   <i class="fa-solid text-xl  fa-circle-question"></i>
-   <i class="fa-solid text-xl fa-volume-high"></i>
+    <i onclick="loadWordDetails(${word.id})" class="fa-solid text-xl  fa-circle-question"></i>
+    <i class="fa-solid text-xl fa-volume-high"></i>
    </div>
   </div>
-   
-</div>
-        `;
+  </div>
+  `;
+  
         wordContainer.appendChild(div);
+        manageSpinner(false);
     });
 }
 
@@ -58,7 +125,7 @@ const displayLesson = (data) => {
     data.forEach(d => {
         const div = document.createElement("div");
         div.innerHTML = `
-            <button onclick="loadLevelWord('${d.level_no}')" class="btn btn-outline btn-secondary hover:text-white my-2">
+            <button id="lesson-btn-${d.level_no}"  onclick="loadLevelWord('${d.level_no}')" class="btn btn-outline lessons-btn btn-secondary  my-2">
                 <i class="fa-solid fa-book"></i> Lessons - ${d.level_no}
             </button>
         `;
