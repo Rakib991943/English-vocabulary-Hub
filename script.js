abd535eadea8268915dbc9cd391275c6e1b25b61
rakib <mdrakib991943@.com>
2026-03-03 07:18:19 +0600

    added new Word

--- script.js
+++ script.js
@@ -12,24 +12,38 @@ const loadLevelWord = async (id) => {
     displayWord(words.data);
 }
 
+
+
 // Display words dynamically
 const displayWord = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
+ if(words.length ===0){
+    wordContainer.innerHTML =`
+     <div class="div text-center col-span-full rounded-xl py-10 space-y-4">
+        <img src='./assets/alert-error.png ' class='mx-auto'> 
+            <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি। </p>
+            <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+        </div>
+    `
+    return ;
+ }
+    words.forEach(word => {
 
-    words.forEach(d => {
         const div = document.createElement("div");
         div.innerHTML = `
             <div class="card bg-base-100 w-96 shadow-sm">
   <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <h2 class="card-title text-xl font-bold">${word.word ? word.word :"sorry"}</h2>
+    <p> Meaning and Pronunciation</>
+    <p class='font-bangla text-xl font-semibold'>${word.meaning  ? word.meaning:"sorry"} || ${word.pronunciation}</p>
+
+    <div class='flex justify-between items-center mt-2'>
+   <i class="fa-solid text-xl  fa-circle-question"></i>
+   <i class="fa-solid text-xl fa-volume-high"></i>
+   </div>
   </div>
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
+   
 </div>
         `;
         wordContainer.appendChild(div);
